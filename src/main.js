ac7078bcc2074b50ce374d43640f0a8cdbcf89c5
guilherme <guilherme@teste.com>
2025-01-09 08:45:21 -0300

    ajustes no mobile

--- src/main.js
+++ src/main.js
@@ -1,7 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
     const buttons = document.querySelectorAll('[data-tab-button]');
-    const tabsContainer = document.querySelectorAll('[data-tab-id]');
+    const questions = document.querySelectorAll('[data-faq-question]');
+
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function() {
+        const posicaoAtual = window.scrollY;
+
+        if (posicaoAtual < alturaHero) {
+            ocultaElementosDoHeader();
+        }
+        else {
+            exibeElementosDoHeader();
+        }
+    })
     
+    // Seção de atrações, programação das abas
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao) {
             const abaAlvo = botao.target.dataset.tabButton;
@@ -12,8 +27,14 @@ document.addEventListener("DOMContentLoaded", function () {
             botao.target.classList.add('shows__tabs__button--is-active');
         })
     }
+
+    //seção faq accordion    
+    for (let i = 0; i < questions.length; i ++) {
+        questions[i].addEventListener('click', abreOuFechaResposta);
+    }
 })
 
+// Função para remover o botão ativo da seção de seleções
 function removeBotaoAtivo() {
     const buttons = document.querySelectorAll('[data-tab-button]');
 
@@ -22,10 +43,31 @@ function removeBotaoAtivo() {
     }
 }
 
+// Esconde os outros titulos visiveis de acordo com o botão selecionado
 function escondeTodasAbas() {
     const tabsContainer = document.querySelectorAll('[data-tab-id]');
 
     for (let i = 0; i < tabsContainer.length; i++) {
         tabsContainer[i].classList.remove('shows__list--is-active');
     }
+}
+
+// Função para abrir e fechar a parte das perguntas e respotas do FAQ
+function abreOuFechaResposta(elemento) {
+    const classe = 'faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe);
+}
+
+// Função para adicionar a classe ao elemento Header
+function ocultaElementosDoHeader() {
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+// Função para remover a classe do Header
+function exibeElementosDoHeader() {
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
 }
